51b4e612d31274f8dbb2f136a3276c8dfcdd171e
Marko Petrovic <gitbluf@tuta.io>
2024-03-01 19:19:33 +0100

    update example versions

--- examples/runner/versions.tf
+++ examples/runner/versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "gitlabhq/gitlab"
       version = ">= 16.0.0"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.37.0"
+    }
   }
   required_version = ">= 1.3.0"
 }
